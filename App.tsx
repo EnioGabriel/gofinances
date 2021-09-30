4eeb451d574cf04ea5f1b2f279b8e26d72af5849
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-29 23:10:35 -0300

    Criando estilos globais

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+// Contexto para disponilizar o tema
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/global/styles/theme";
+
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
