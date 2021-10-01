cd1e8e9f656b49abe1af4a92d335207cd7acf912
Enio Gabrel <enio@nexti.com>
2021-10-01 16:01:14 -0300

    Utilizando icones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-
-import { Container, Title } from "./styles";
+import { Container, Header, UserWrapper, UserImage, UserGreetings, Greetings, Icon, UserName } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserImage source={{ uri: 'https://avatars.githubusercontent.com/u/38878246?v=4' }} />
+          <UserGreetings>
+            <Greetings>Olá,</Greetings>
+            <UserName>Gabriel</UserName>
+          </UserGreetings>
+        </UserWrapper>
+
+        {/* Chamando VectorIcon estilizado e passando o nome como props */}
+        <Icon name='power' />
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,18 +1,59 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from '@expo/vector-icons'
+
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  /* Alinhando na vertical */
-  justify-content: center;
-  /* Alinhando na horizontal */
-  align-items: center;
-
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  align-items: center;
+  flex-direction: row;
 `;
+
+export const UserWrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  
+  /* padding: 0 24px; */
+
+  flex-direction: row;
+  /* justify-content: space-between; */
+  /* align-items: center; */
+
+`;
+
+export const UserImage = styled.Image`
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+  border-radius: 10px;
+  margin-left: 13px;
+`;
+
+export const UserGreetings = styled.View`
+  margin-left: 13px;
+`;
+
+export const Greetings = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+`
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+// Estilizando uma biblioteca (Feather Vector Icon)
+export const Icon = styled(Feather)`
+  padding: 13px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`
