b8930a01ae94bb69402b5cbba7bb0f607dc59321
Enio Gabrel <enio@nexti.com>
2021-10-01 09:24:38 -0300

    Utilizando fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 // Contexto para disponibilizar o tema
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/global/styles/theme";
 
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from "@expo-google-fonts/poppins"
+
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  // Aguarda carregar as fontes (Segura o inicio do App)
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
