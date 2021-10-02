1f89f4c62c0f1cbd8597173d4eec06c86d9f728b
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-10-01 23:01:24 -0300

    Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -32,9 +32,26 @@ export function Dashboard() {
         <Icon name="power" />
       </Header>
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 22 de outubro"
+        />
+
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 11 de outubro"
+        />
+
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
       </HighlightCards>
     </Container>
   );
