a27e250595f3e8bfc1180ffb1e07eca7a018bbe6
Enio Gabrel <enio@nexti.com>
2021-10-19 17:52:30 -0300

    Estilizando a tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { useTheme } from 'styled-components';
+
+import { Dashboard } from '../screens/Dashboard';
+import { Platform } from 'react-native';
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes() {
+   const theme = useTheme();
+
    return (
-      <Navigator>
+      <Navigator
+         screenOptions={{
+            tabBarActiveTintColor: theme.colors.secondary,
+            tabBarInactiveTintColor: theme.colors.text,
+            tabBarLabelPosition: 'beside-icon',
+            tabBarStyle: {
+               paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+               height: 88
+            }
+         }}
+      >
          <Screen
             name="Listagem"
             component={Dashboard}
-            // configurações do Header que é criado por default
+            // Estilizando configurações criadas por default
             options={{
+               tabBarIcon: (({ size, color }) => (
+                  <MaterialIcons
+                     name="format-list-bulleted"
+                     size={size}
+                     color={color}
+                  />
+               )),
                // Desativa o background
                headerTransparent: true,
                // Desativa o texto
@@ -25,6 +48,32 @@ export function AppRoutes() {
             component={Register}
             // configurações do Header que é criado por default
             options={{
+               tabBarIcon: (({ size, color }) => (
+                  <MaterialIcons
+                     name="attach-money"
+                     size={size}
+                     color={color}
+                  />
+               )),
+               // Desativa o background
+               headerTransparent: true,
+               // Desativa o texto
+               headerStatusBarHeight: 0
+            }}
+         />
+
+         <Screen
+            name="Resumo"
+            component={Register}
+            // configurações do Header que é criado por default
+            options={{
+               tabBarIcon: (({ size, color }) => (
+                  <MaterialIcons
+                     name="pie-chart"
+                     size={size}
+                     color={color}
+                  />
+               )),
                // Desativa o background
                headerTransparent: true,
                // Desativa o texto
