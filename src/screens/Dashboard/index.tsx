4662baa56e8cc4b80287eb825c4cbef63ec6bfbc
Enio Gabriel <enio@nexti.com>
2021-12-17 16:34:57 -0300

    Finalizando a lista

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useCallback, useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 
 import { HighlightCard } from "../../components/HighlightCard";
@@ -61,6 +62,11 @@ export function Dashboard() {
   useEffect(() => {
     loadTransactions();
   }, [])
+
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  }, []));
 
   return (
     <Container>
